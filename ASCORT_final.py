9ad0bd07879ff57b13d5bcbfd4ab005f3b196dcb
bgvinayk <101205129+bgvinayk@users.noreply.github.com>
2022-07-18 00:55:15 +0530

    Update ASCORT_final.py

--- ASCORT_final.py
+++ ASCORT_final.py
@@ -874,7 +874,7 @@ def catanion(pHest, Malk_molar):
 #     conditions.pressure(P, "atm")
 #     conditions.pH(pHest)
 #     result = solver.solve(state, conditions)
-    print(state)
+    print(system)
     return state
 
 def deltax(xe,Ca,PO4,SI):
